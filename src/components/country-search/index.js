3a069aedfaea040f46e2dffd678e04ed0397c26d
Will Nippard <William_Nippard@Manulife.ca>
2022-08-30 17:10:30 -0400

    Remove solution

--- src/components/country-search/index.js
+++ src/components/country-search/index.js
@@ -4,18 +4,6 @@ import "./index.css";
 // While paginated API, only need 1st page for this
 
 const CountrySearch = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [data, setData] = useState({});
-
-  const onClick = () => {
-    fetch(`https://jsonmock.hackerrank.com/api/countries?name=${searchTerm}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data.data[0]);
-        setSearchTerm("");
-      });
-  };
-
   return (
     <div className="mt-75 layout-column justify-content-center align-items-center">
       <section className="layout-row align-items-center justify-content-center">
@@ -24,37 +12,14 @@ const CountrySearch = () => {
           className="large"
           placeholder="Country"
           data-testid="app-input"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button
-          type="submit"
-          className="ml-30"
-          data-testid="submit-button"
-          onClick={onClick}
-        >
+        <button type="submit" className="ml-30" data-testid="submit-button">
           Search Country
         </button>
       </section>
 
-      {data && data.name && (
-        <h1 className="styled mt-50" data-testid="country-name">
-          {data.name}
-        </h1>
-      )}
-      {data &&
-        data.borders &&
-        data.borders.map((border, index) => {
-          return (
-            <p
-              className="styled mt-50"
-              data-testid="country-border"
-              key={index}
-            >
-              {border}
-            </p>
-          );
-        })}
+      <h1 className="styled mt-50" data-testid="country-name"></h1>
+      <p className="styled mt-50" data-testid="country-border"></p>
     </div>
   );
 };
